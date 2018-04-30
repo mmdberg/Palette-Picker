479271fa17d17f790ae158690adced6914fd3d1b
Maddy Dannenberg <mmdberg@gmail.com>
2018-04-30 14:36:49 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'))
+
+app.set('port', process.env.PORT || 3000);
+
+app.locals.title = 'Palette Picker'
+
+app.get('/', (request, response) => {
+
+})
+
+
+
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}`)
+})
+
+
+
