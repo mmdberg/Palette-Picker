3df6ef348c7c07795e893f995f117b094949a0ac
Maddy Dannenberg <mmdberg@gmail.com>
2018-05-02 09:14:58 -0600

    Add error if project name already exists

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -82,7 +82,6 @@ const savePalette = async () => {
   })
   const paletteResponse = await response.json()
   const paletteID = paletteResponse.id
-  console.log(paletteID)
   $(`#${project_id}`).after(
   `<article class="palette-list">
     <p>${paletteName}</p>
@@ -96,20 +95,29 @@ const savePalette = async () => {
 
 const saveProject = async () => {
   const projectName = ($('.project-name-input').val())
-  const response = await fetch('/api/v1/projects', {
-    method: 'POST',
-    body: JSON.stringify({
-      title: projectName
-    }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  const projectResponse = await response.json()
-  const projectID = projectResponse.id
-  $('.project-list').append(`<h4>${projectName}<h4>
-    <p class="no-palettes-saved">No palattes saved for this project<p>`)
-  $('.project-options').append(`<option value="${projectID}">${projectName}</option>`)
+  const response = await fetch('/api/v1/projects')
+  const projects = await response.json()
+  const projectMatch = projects.find(project => project.title === projectName)
+  if (projectMatch) {
+    $('.save-project-button').after('<p class="project-match">That project name already exists</p>')
+  } else {
+    $('.project-match').remove()
+    const response = await fetch('/api/v1/projects', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: projectName
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const projectResponse = await response.json()
+    const projectID = projectResponse.id
+    $('.project-list').append(`<h4>${projectName}<h4>
+      <p class="no-palettes-saved">No palattes saved for this project<p>`)
+    $('.project-options').append(`<option value="${projectID}">${projectName}</option>`)
+  }
+  ($('.project-name-input').val(''))
 }
 
 const getPalettes = async (id) => {
@@ -133,7 +141,6 @@ const makeArray = (projects) => {
 
 const makeDropdown = (projectArray) => {
   projectArray.forEach(project => {
-    console.log('project:', project)
     $('.project-options').append(`<option value="${project.id}">${project.title}</option>`)
   })
 }
